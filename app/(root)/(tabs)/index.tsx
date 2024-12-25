bf4441258e8d55ef29a1a689ad77b916c24d3e60
Sai Vara Prasad Gandhe <gsaivaraprasad123@gmail.com>
2024-12-25 22:57:20 +0530

    Implement navbar

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -14,10 +14,6 @@ export default function Index() {
       <Text className="font-bold text-lg my-10 font-rubik">
         Welcome to ReState
       </Text>
-      <Link href="/sign-in">Sign-in</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/31">Properties</Link>
     </View>
   );
 }
